678d1a23398265e6d40a59b231ca3c59625d35d4
garawiyudha <gara@majoo.id>
2021-04-01 17:08:50 +0700

    init main layout

--- app/src/main/java/com/gara/findgithubuserapp/MainActivity.kt
+++ app/src/main/java/com/gara/findgithubuserapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.gara.findgithubuserapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bt_search.setOnClickListener {
+            var name = et_name.text
+            
+        }
+
     }
 }
